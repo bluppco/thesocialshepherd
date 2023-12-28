4853105048de2ad918b38ab0cd200dfd9202fc7b
Mohit <mohit@hey.com>
2023-12-28 15:16:03 +0530

    x

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+
 			colors: {
 
 				"primary": "#65E2CA",
 
+			},
+			fontFamily: {
+
+				"gilmer": ["Gilmer", "sans"],
+				"postbox": ["Post Box", "sans"],
+
 			}
+
 		}
 	},
 	plugins: [],
