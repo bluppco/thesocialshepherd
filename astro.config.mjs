5d22441cd63f58a8f3798a2ea5d86cdf233d7beb
Mohit <mohit@hey.com>
2023-12-28 14:25:55 +0530

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,10 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [mdx(), tailwind(), react()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({
+    runtime: {
+      mode: 'local',
+      type: 'pages'
+    }
+  })
 });
